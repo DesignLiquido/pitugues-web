d8e1480c3a02c9746a9d863085299ed51bebf09d
Maite <maicapref@gmail.com>
2025-07-18 09:51:00 -0300

    Adicionando mais palavras reservadas na costante KEY_WORDS_DOCS_LIST

--- src/constants.js
+++ src/constants.js
@@ -59,8 +59,80 @@ export const KEYWORDS_DOCS_LIST = [
         nome: 'minusculo',
         documentacao: 'Converte todos os caracteres de um texto para minúsculo',
     },
-    // {
-    //     nome: ,
-    //     documentacao: ,
-    // },
+    {
+        nome: 'substituir',
+        documentacao: 'Substitui valores textuais indicados por parâmetros, sendo o primeiro o valor a ser substituído e o segundo o valor substituto',
+    },
+    {
+        nome: 'subtexto',
+        documentacao: 'Extrai o texto entre as posições indicadas nos parâmetros',
+    },
+    {
+        nome: 'arredondarParaBaixo',
+        documentacao: 'Arredonda valor numérico para baixo.',
+    },
+    {
+        nome: 'empilhar',
+        documentacao: 'Adiciona valores a um vetor seguindo a estrutura de pilha',
+    },
+    {
+        nome: 'inverter',
+        documentacao: 'Retorna valores invertidos de um vetor',
+    },
+    {
+        nome: 'juntar',
+        documentacao: 'União de valores a partir de um separador indicado',
+    },
+    {
+        nome: 'ordenar',
+        documentacao: 'Ordena valores de um array pelo padrão crescente e/ou alfabética',
+    },
+    {
+        nome: 'remover',
+        documentacao: 'Remove o elemento indicado nos argumentos',
+    },
+    {
+        nome: 'removerPrimeiro',
+        documentacao: 'Remove o primeiro elemento de um vetor',
+    },
+    {
+        nome: 'removerUltimo',
+        documentacao: 'Remove o último elemento de um vetor',
+    },
+    {
+        nome: 'somar',
+        documentacao: 'Soma os valores de um vetor numérico',
+    },
+    {
+        nome: 'tamanho',
+        documentacao: 'Retorna o tamanho de um vetor ou texto',
+    },
+    {
+        nome: 'aleatorio',
+        documentacao: 'Retorna um valor numérico aleatório',
+    },
+    {
+        nome: 'aleatorioEntre',
+        documentacao: 'Retorna um valor numérico aleatório entre dois valores indicados nos argumentos',
+    },
+    {
+        nome: 'inteiro',
+        documentacao: 'Converte valores textuais e númericos reais para um valor númerico do tipo inteiro',
+    },
+    {
+        nome: 'numero',
+        documentacao: 'Converte valores textuais para tipo número',
+    },
+    {
+        nome: 'número',
+        documentacao: 'Converte valores textuais para tipo número',
+    },
+    {
+        nome: 'real',
+        documentacao: 'Converte valores textuais e númericos inteiros para um valor númerico do tipo real',
+    },
+    {
+        nome: 'texto',
+        documentacao: 'Converte valores para tipo texto',
+    },
 ]
