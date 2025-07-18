e3302ed72768fa1a2ea27ae95180ec536879ff88
Maite <maicapref@gmail.com>
2025-07-18 09:25:24 -0300

    Refatorando listagem para syntax highlighting e adicionando autocomplete para os métodos de pituguês

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,62 @@ export const LANGUAGE_VERSION = {
 export const CODE_SNIPPETS = {
     pitugues: `escreva("Olá, Design Líquido!")\n`
 };
+
+export const KEYWORDS_DOCS_LIST = [
+    {
+        nome: 'adicionar',
+        documentacao: 'Adiciona um item à lista.'
+    }, 
+    {
+        nome: 'arredondarParaCima',
+        documentacao: 'Arredonda valor numérico para cima.'
+    },
+    {
+        nome: 'escreva',
+        documentacao: 'Método que retorna um valor textual' ,
+    },
+    {
+        nome: 'leia',
+        documentacao: 'Função de entrada para valores textuais',
+    },
+    {
+        nome: 'aparar',
+        documentacao: 'Método textual que apara as extremidades de um texto',
+    },
+    {
+        nome: 'apararFim',
+        documentacao: 'Método textual que apara o final de um texto',
+    },
+    {
+        nome: 'apararInicio',
+        documentacao: 'Método textual que apara o início de um texto',
+    },
+    {
+        nome: 'concatenar',
+        documentacao: 'Concatenação de elementos textuais',
+    },
+    {
+        nome: 'dividir',
+        documentacao: 'Gera vetor a partir de valores textuais com indicação de separador',
+    },
+    {
+        nome: 'fatiar',
+        documentacao: 'Retorna os valores de um texto de acordo com as posições indicadas',
+    },
+    {
+        nome: 'inclui',
+        documentacao: 'Verifica se o valor indicado está presente no texto',
+    },
+    {
+        nome: 'maiusculo',
+        documentacao: 'Converte todos os caracteres de um texto para maiúsculo',
+    },
+    {
+        nome: 'minusculo',
+        documentacao: 'Converte todos os caracteres de um texto para minúsculo',
+    },
+    // {
+    //     nome: ,
+    //     documentacao: ,
+    // },
+]
