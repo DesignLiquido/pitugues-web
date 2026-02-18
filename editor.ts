a4ce388828782ce3de0a936f3b27ddf8c38c6204
João Lucas Carvalho <88744662+Joaolpridolficarvalho@users.noreply.github.com>
2026-02-18 12:46:14 -0300

    Correção do registerCompletionItemProvider (#17)
    * Correção do registerCompletionItemProvider
    * atualiza yarn.lock e exclui package-lock.json

--- editor.ts
+++ editor.ts
@@ -257,7 +257,6 @@ function definirLinguagemPitugues() {
       'tente',
       'texto[]',
       'tipo',
-      'var',
       'variavel',
       'variável',
       'verdadeiro',
@@ -746,10 +745,11 @@ const configurarLinguagemPitugues = function () {
     Monaco.languages.registerCompletionItemProvider('pitugues', {
         provideCompletionItems: () => {
             const formatoPrimitivas = primitivas.filter(p => p.exemploCodigo).map(({ nome, exemploCodigo: exemplo }) => {
+                 const insertText = exemplo.includes('.') ? exemplo.split('.')[1] : exemplo;
                 return {
                     label: nome,
                     kind: 17, // Keyword,
-                    insertText: exemplo.split('.')[1],
+                    insertText: insertText || nome,
                     insertTextRules: 4 // InsertAsSnippet
                 }
             });
